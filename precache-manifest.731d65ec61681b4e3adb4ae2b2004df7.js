2b28ffb6c9f28e07f90042a75e70c320bf815594
rawanah995 <rawanah995@gmail.com>
2019-04-13 00:04:07 +0300

    Updates

---
+++ precache-manifest.731d65ec61681b4e3adb4ae2b2004df7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "73e353197a77fdf3a8d8",
+    "url": "/react-project4/static/css/main.d6914543.chunk.css"
+  },
+  {
+    "revision": "73e353197a77fdf3a8d8",
+    "url": "/react-project4/static/js/main.833a7658.chunk.js"
+  },
+  {
+    "revision": "5319a32990aa7e77ae16",
+    "url": "/react-project4/static/js/runtime~main.d06ebd11.js"
+  },
+  {
+    "revision": "bb4e24d1f19904d8668b",
+    "url": "/react-project4/static/css/2.d17585fa.chunk.css"
+  },
+  {
+    "revision": "bb4e24d1f19904d8668b",
+    "url": "/react-project4/static/js/2.461768ac.chunk.js"
+  },
+  {
+    "revision": "b846e001bb06fac370a9b91d64077a9c",
+    "url": "/react-project4/index.html"
+  }
+];
